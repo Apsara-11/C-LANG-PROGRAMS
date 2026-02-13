d6ad4882b5e51228e681ab672790f7074e9e08b8
Apsara-11 <arapsara11@gmail.com>
2026-02-13 09:17:53 +0530

    Update even and odd count program

--- array/count_even_and_odd_numbers.c
+++ array/count_even_and_odd_numbers.c
@@ -1,20 +1,20 @@
-#include <stdio.h>
-int main() {
-    int a[100], n, i, even=0, odd=0;
+#include<stdio.h>
+int main()
+{
+    int a[100],n,i,even=0,odd=0;
 
     scanf("%d",&n);
-
-    for(i=0;i<n;i++)
+    
+    for(i=0;i<n;i++){
         scanf("%d",&a[i]);
+    }
 
-    for(i=0;i<n;i++) {
+    for(i=0;i<n;i++){
         if(a[i]%2==0)
             even++;
         else
             odd++;
     }
 
-    printf("Even=%d Odd=%d",even,odd);
-    return 0;
+    printf("Even:%d and Odd:%d",even,odd);
 }
-
