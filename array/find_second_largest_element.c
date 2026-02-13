1e4dcdc2851f7959037a7343043814b120296898
Apsara-11 <arapsara11@gmail.com>
2026-02-13 09:53:52 +0530

    C Daily Practice

--- array/find_second_largest_element.c
+++ array/find_second_largest_element.c
@@ -1,25 +1,32 @@
-#include <stdio.h>
-int main() {
-    int a[100], n, i;
-    int largest, second;
+#include<stdio.h>
+int main()
+{
+    int a[100],i,n,fir,sec,temp=0;
 
     scanf("%d",&n);
 
-    for(i=0;i<n;i++)
+    for(i=0;i<n;i++){
         scanf("%d",&a[i]);
+    }
 
-    largest = second = -99999;   
+    fir=a[0];
+    sec=a[1];
 
-    for(i=0;i<n;i++) {
-        if(a[i] > largest) {
-            second = largest;
-            largest = a[i];
+    if(sec>fir){
+        temp=fir;
+        fir=sec;
+        sec=temp;
+    }
+
+    for(i=0;i<n;i++){
+        if(a[i]>fir){
+            sec=fir;
+            fir=a[i];
         }
-        else if(a[i] > second && a[i] != largest) {
-            second = a[i];
+        else if(a[i]>sec && a[i]!=fir){
+            sec=a[i];
         }
     }
 
-    printf("Second Largest = %d", second);
-    return 0;
+    printf("First = %d and Second = %d",fir,sec);
 }
